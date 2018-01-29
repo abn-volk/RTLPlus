8b2189079b1f5aacefe704dfe7db15be6f6a66af
abn-volk <pnhoang96@gmail.com>
2018-01-29 23:56:06 +0700

    Implement actions for synchronisation transformations

--- src/org/uet/dse/rtlplus/actions/ActionFindMatches.java
+++ src/org/uet/dse/rtlplus/actions/ActionFindMatches.java
@@ -2,6 +2,7 @@ package org.uet.dse.rtlplus.actions;
 
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -23,6 +24,7 @@ import org.uet.dse.rtlplus.matching.ForwardMatchManager;
 import org.uet.dse.rtlplus.matching.IntegrationMatchManager;
 import org.uet.dse.rtlplus.matching.Match;
 import org.uet.dse.rtlplus.matching.MatchManager;
+import org.uet.dse.rtlplus.mm.MRuleCollection.TransformationType;
 
 import com.google.common.eventbus.EventBus;
 
@@ -30,6 +32,10 @@ public class ActionFindMatches implements IPluginActionDelegate {
 
 	@Override
 	public void performAction(IPluginAction pluginAction) {
+		if (Main.getTggRuleCollection().getRuleList().size() == 0) {
+			JOptionPane.showMessageDialog(pluginAction.getParent(), "Please open a TGG rule file first.", "No rules available", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
 		MainWindow mainWindow = pluginAction.getParent();
 		PrintWriter logWriter = mainWindow.logWriter();
 		MSystemState state = pluginAction.getSession().system().state();
@@ -51,49 +57,56 @@ public class ActionFindMatches implements IPluginActionDelegate {
 			manager = null;
 			break;
 		}
-		if (manager != null) {
-			MatchListDialogResult res = new MatchListDialogResult();
-			List<Match> matches = manager.findMatchesForRules(Main.getTggRuleCollection().getRuleList());
-			if (matches.isEmpty())
-				JOptionPane.showMessageDialog(mainWindow, "No matches were found.");
-			else {
-				EventBus eventBus = pluginAction.getSession().system().getEventBus();
-				URL url = Main.class.getResource("/resources/list.png");
-				ViewFrame vf = new ViewFrame("Match list", null, "");
-				vf.setFrameIcon(new ImageIcon(url));
-				MatchListDialog dialog = new MatchListDialog(vf, matches, eventBus, state, logWriter, res);
-				vf.addInternalFrameListener(new InternalFrameListener() {
-					@Override
-					public void internalFrameActivated(InternalFrameEvent arg0) {
-					}
-					@Override
-					public void internalFrameClosed(InternalFrameEvent arg0) {	
-						if (res.isResult())
-							performAction(pluginAction);
-					}
-					@Override
-					public void internalFrameClosing(InternalFrameEvent arg0) {						
-					}
-					@Override
-					public void internalFrameDeactivated(InternalFrameEvent arg0) {						
-					}
-					@Override
-					public void internalFrameDeiconified(InternalFrameEvent arg0) {						
-					}
-					@Override
-					public void internalFrameIconified(InternalFrameEvent arg0) {						
-					}
-					@Override
-					public void internalFrameOpened(InternalFrameEvent arg0) {						
-					}
-					
-				});
-				vf.setContentPane(dialog);
-				vf.pack();
-				mainWindow.addNewViewFrame(vf);
-			}
+		List<Match> matches = new ArrayList<>();
+		if (Main.getTggRuleCollection().getType() == TransformationType.SYNCHRONIZATION) {
+			MatchManager forwardManager = new ForwardMatchManager(state, false);
+			matches.addAll(forwardManager.findMatches());
+			MatchManager backwardManager = new BackwardMatchManager(state, false);
+			matches.addAll(backwardManager.findMatches());
+			MatchManager integrationManager = new IntegrationMatchManager(state, false);
+			matches.addAll(integrationManager.findMatches());
 		}
-		else JOptionPane.showMessageDialog(mainWindow, "This transformation type is unsupported.", "Unsupported transformation type", JOptionPane.ERROR_MESSAGE);
+		else
+			matches = manager.findMatches();
+		MatchListDialogResult res = new MatchListDialogResult();
+		if (matches.isEmpty())
+			JOptionPane.showMessageDialog(mainWindow, "No matches were found.");
+		else {
+			EventBus eventBus = pluginAction.getSession().system().getEventBus();
+			URL url = Main.class.getResource("/resources/list.png");
+			ViewFrame vf = new ViewFrame("Match list", null, "");
+			vf.setFrameIcon(new ImageIcon(url));
+			MatchListDialog dialog = new MatchListDialog(vf, matches, eventBus, state, logWriter, res);
+			vf.addInternalFrameListener(new InternalFrameListener() {
+				@Override
+				public void internalFrameActivated(InternalFrameEvent arg0) {
+				}
+				@Override
+				public void internalFrameClosed(InternalFrameEvent arg0) {	
+					if (res.isResult())
+						performAction(pluginAction);
+				}
+				@Override
+				public void internalFrameClosing(InternalFrameEvent arg0) {						
+				}
+				@Override
+				public void internalFrameDeactivated(InternalFrameEvent arg0) {						
+				}
+				@Override
+				public void internalFrameDeiconified(InternalFrameEvent arg0) {						
+				}
+				@Override
+				public void internalFrameIconified(InternalFrameEvent arg0) {						
+				}
+				@Override
+				public void internalFrameOpened(InternalFrameEvent arg0) {						
+				}
+				
+			});
+			vf.setContentPane(dialog);
+			vf.pack();
+			mainWindow.addNewViewFrame(vf);
+		}
 	}
 
 }
